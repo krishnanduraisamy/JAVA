e1f7a99f136ecaa4eb2650a82d2ba79ab296a982
krishnanduraisamy <105281973+krishnanduraisamy@users.noreply.github.com>
2022-05-10 15:37:54 +0530

    Add files via upload
    Java Programs

---
+++ classA.java
@@ -0,0 +1,20 @@
+
+public class classA {
+	
+	int i;
+	float f;
+	
+
+
+public classA() {
+	
+	i=5;
+	f=5.5f;
+	
+	System.out.println("value is " +i  + f );
+	
+}
+  
+     
+
+}
---
+++ kris2.java
@@ -0,0 +1,15 @@
+
+public class kris2 {
+
+	public static void main(String[] args) {
+ 
+		for(int i=1;i<=10;i++) {
+			if(i==7) {
+				continue;
+			}
+			
+			System.out.println("value is"  +i);
+		}
+	}
+
+}
